aaa07c57572e316edb2d1e2198e9f808276b4de7
hakimdaniel <mika259123321@gmail.com>
2025-03-11 23:04:43 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -72,7 +72,7 @@ function App() {
       <Router basename="/my-react-portfolio">
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
           <div className="container-fluid">
-            <Link className="navbar-brand" to="/#/">H4K1M</Link>
+            <Link className="navbar-brand" to="/">H4K1M</Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
